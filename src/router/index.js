73959cca3d7c8dfe47c67ca93d15c841a8280003
ymktmk <ymktmk.co@gmail.com>
2021-12-07 19:28:15 +0900

    # delete

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import TopPage from '@/components/TopPage'
+import TopPage from '@/components/TopPage'
 import Python from '@/components/Python'
 import PHP from '@/components/PHP'
 
 Vue.use(Router)
 
 const router = new Router({
+    mode: 'history',
     routes: [
-    // {
-    //     path: '/',
-    //     name: 'TopPage',
-    //     component: TopPage
-    // },
+    {
+        path: '/',
+        name: 'TopPage',
+        component: TopPage
+    },
     {
         path: '/python',
         name: 'Python',
